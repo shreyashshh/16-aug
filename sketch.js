cd59fd6fc101c355b65b89b9621d8afb5f223e00
shreyashshh <shreyashshrivastav76@gmail.com>
2021-08-16 17:53:17 +0530

    16 aug

--- sketch.js
+++ sketch.js
@@ -17,6 +17,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
+    platform=new Ground(150,300,300,170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -48,6 +49,7 @@ function draw(){
     ground.display();
     pig1.display();
     log1.display();
+    platform.display();
 
     box3.display();
     box4.display();
@@ -59,4 +61,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
 }
